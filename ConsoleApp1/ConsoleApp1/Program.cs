e214b74306cab195efc01e760517d04b93ec50bd
umberto-github <bachetti.u@gmail.com>
2025-12-26 16:00:25 +0100

    5

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,7 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
+
 Console.WriteLine("Hello, World!");
 
 Console.WriteLine("1");
 Console.WriteLine("2");
 Console.WriteLine("3");
 Console.WriteLine("4");
+Console.WriteLine("5");
